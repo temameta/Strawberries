d8e8e65794476e9e3066c81954ae573261012286
tema <sezemov2@gmail.com>
2026-04-15 12:12:45 +0300

    feat: add validation annotation and validator for phone number

---
+++ user-service/user-rest-api-contract/src/main/java/org/strawberries/userrestapicontract/validation/PhoneValidator.java
@@ -0,0 +1,16 @@
+package org.strawberries.userrestapicontract.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.regex.Pattern;
+
+public class PhoneValidator implements ConstraintValidator<Phone, String> {
+    private static final Pattern CORRECT_PHONE = Pattern.compile("^\\+7\\d{10}$");
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        if (s == null || s.isBlank()) return true;
+        if (s.length() != 12) return false;
+        return CORRECT_PHONE.matcher(s).matches();
+    }
+}
