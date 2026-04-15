acb011534f1cf55a013d81b2a265ce3f3adc3388
tema <sezemov2@gmail.com>
2026-04-15 12:12:17 +0300

    feat: declarated user request

--- user-service/user-rest-api-contract/src/main/java/org/strawberries/userrestapicontract/dto/UserRequest.java
+++ user-service/user-rest-api-contract/src/main/java/org/strawberries/userrestapicontract/dto/UserRequest.java
@@ -1,4 +1,23 @@
 package org.strawberries.userrestapicontract.dto;
 
-public class UserRequest {
-}
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.strawberries.userrestapicontract.validation.Phone;
+
+public record UserRequest(
+    @Schema(description = "Email пользователя", example = "example@gmail.com")
+    @Email(message = "Некорректный email")
+    @Size(max = 255, message = "Email не может превышать 255 символов")
+    String email,
+    @Schema(description = "Имя пользователя", example = "Иван", requiredMode = Schema.RequiredMode.REQUIRED)
+    @NotBlank(message = "Имя не может быть пустым")
+    @Size(max = 255, message = "Имя не может превышать 255 символов")
+    String firstName,
+    @Schema(description = "Номер телефона пользователя", example = "+79998886655", requiredMode = Schema.RequiredMode.REQUIRED)
+    @Phone
+    String phone,
+    @Schema(description = "Роль пользователя", example = "USER")
+    Role role
+) {}
