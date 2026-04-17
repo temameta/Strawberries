c4f07893d6b10d9e86988c31f4a6a3217a47e130
tema <sezemov2@gmail.com>
2026-04-17 17:34:08 +0300

    feat: make ResourceNotFoundException UserNotFoundException

--- api/user-api/src/main/java/org/strawberries/userapi/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.strawberries.userapi.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String resourceName, Object resourceId) {
-        super(String.format("%s with id=%s not found", resourceName, resourceId));
-    }
-}
---
+++ api/user-api/src/main/java/org/strawberries/userapi/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package org.strawberries.userapi.exception;
+
+import java.util.UUID;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(UUID id) {
+        super(String.format("User with id=%s not found", id));
+    }
+}
