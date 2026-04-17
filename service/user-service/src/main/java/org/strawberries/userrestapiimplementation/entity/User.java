2ac308be9522e4ad6fe0d0681476b653b9d85fd4
tema <sezemov2@gmail.com>
2026-04-17 16:19:42 +0300

    feat: add user entity

--- service/user-service/src/main/java/org/strawberries/userrestapiimplementation/entity/User.java
+++ service/user-service/src/main/java/org/strawberries/userrestapiimplementation/entity/User.java
@@ -1,9 +1,33 @@
 package org.strawberries.userrestapiimplementation.entity;
 
-public class User {
-    private Long id;
-    private String firstName;
-    private String email;
-    private String phone;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.strawberries.userrestapicontract.dto.Role;
 
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private Long id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column
+    private String email;
+    @Column(unique = true, nullable = false)
+    private String phone;
+    @Column(nullable = false)
+    private Role role;
+    @Column(nullable = false)
+    private boolean isActive;
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    @Column
+    private LocalDateTime updatedAt;
 }
