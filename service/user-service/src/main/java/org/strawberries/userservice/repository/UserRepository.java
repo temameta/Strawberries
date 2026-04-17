6375a610464840710f389c3202d6fe1c7ad16204
tema <sezemov2@gmail.com>
2026-04-17 22:44:40 +0300

    feat: add methods to find entries by isActive field

--- service/user-service/src/main/java/org/strawberries/userservice/repository/UserRepository.java
+++ service/user-service/src/main/java/org/strawberries/userservice/repository/UserRepository.java
@@ -1,11 +1,18 @@
 package org.strawberries.userservice.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.strawberries.userservice.entity.User;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Page<User> findAllByIsActive(Pageable pageable, boolean isActive);
+    Optional<User> findByIdAndIsActiveTrue(UUID id);
+    boolean existsByEmailAndIsActiveTrue(String email);
+    boolean existsByPhoneAndIsActiveTrue(String phone);
 }
