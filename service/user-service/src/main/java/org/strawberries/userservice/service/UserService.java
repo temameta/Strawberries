3685440a7258f6179b6490088c581698e25832ef
tema <sezemov2@gmail.com>
2026-04-17 22:43:18 +0300

    feat: make interface for service and implementation

--- service/user-service/src/main/java/org/strawberries/userservice/service/UserService.java
+++ service/user-service/src/main/java/org/strawberries/userservice/service/UserService.java
@@ -1,28 +1,17 @@
 package org.strawberries.userservice.service;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.strawberries.userapi.dto.Role;
+import org.strawberries.userapi.dto.PagedResponse;
+import org.strawberries.userapi.dto.UserPatchRequest;
 import org.strawberries.userapi.dto.UserRequest;
 import org.strawberries.userapi.dto.UserResponse;
-import org.strawberries.userservice.entity.User;
-import org.strawberries.userservice.repository.UserRepository;
 
-@Service
-@RequiredArgsConstructor
-public class UserService {
-    private final UserRepository repository;
+import java.util.UUID;
 
-    // create findbyid findall delete patch update
-
-    public UserResponse create(UserRequest request) {
-        repository.save(User.builder()
-                            .email(request.email())
-                            .firstName(request.firstName())
-                            .phone(request.phone())
-                            .role(request.role() == null ? Role.USER : Role.ADMIN)
-                            .isActive(true)
-                            .build());
-        return UserResponse.builder().id(request.).email().firstName().phone().role().isActive().createdAt().updatedAt().build();
-    }
+public interface UserService {
+    UserResponse create(UserRequest request);
+    PagedResponse<UserResponse> findAll(int page, int size, Boolean isActive);
+    UserResponse findById(UUID id, boolean onlyActive);
+    UserResponse update(UUID id, UserRequest request);
+    UserResponse patch(UUID id, UserPatchRequest request);
+    void delete(UUID id);
 }
