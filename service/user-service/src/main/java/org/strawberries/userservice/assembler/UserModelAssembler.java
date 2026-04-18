5e93dc58a258b9119fc22239415e4c2ed2ac04ce
tema <sezemov2@gmail.com>
2026-04-18 17:09:32 +0300

    feat: add realization to assembler

--- service/user-service/src/main/java/org/strawberries/userservice/assembler/UserModelAssembler.java
+++ service/user-service/src/main/java/org/strawberries/userservice/assembler/UserModelAssembler.java
@@ -1,4 +1,23 @@
 package org.strawberries.userservice.assembler;
 
-public class UserModelAssembler {
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+import org.strawberries.userapi.dto.UserResponse;
+import org.strawberries.userservice.controller.UserController;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class UserModelAssembler implements RepresentationModelAssembler<UserResponse, EntityModel<UserResponse>> {
+
+    @Override
+    public EntityModel<UserResponse> toModel(UserResponse user) {
+        return EntityModel.of(user,
+                linkTo(methodOn(UserController.class).getUserById(user.getId())).withSelfRel(),
+                linkTo(methodOn(UserController.class).getAllUsers(0, 20)).withRel("users")
+                
+        );
+    }
 }
