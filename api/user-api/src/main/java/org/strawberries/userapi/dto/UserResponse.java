88f47569674140bf7718644908cd53267180d5c1
tema <sezemov2@gmail.com>
2026-04-18 17:09:52 +0300

    fix: make all fields not final

--- api/user-api/src/main/java/org/strawberries/userapi/dto/UserResponse.java
+++ api/user-api/src/main/java/org/strawberries/userapi/dto/UserResponse.java
@@ -18,19 +18,19 @@ import java.util.UUID;
 @Schema(description = "Информация о пользователе")
 public class UserResponse {
     @Schema(description = "ID пользователя", example = "1")
-    private final UUID id;
+    private UUID id;
     @Schema(description = "Email пользователя", example = "example@gmail.com")
-    private final String email;
+    private String email;
     @Schema(description = "Имя пользователя", example = "Иван")
-    private final String firstName;
+    private String firstName;
     @Schema(description = "Номер телефона пользователя", example = "+79998886655")
-    private final String phone;
+    private String phone;
     @Schema(description = "Роль пользователя", example = "USER")
-    private final Role role;
+    private Role role;
     @Schema(description = "Активен пользователь или нет (неактивен = удален)", example = "true")
-    private final boolean isActive;
+    private boolean isActive;
     @Schema(description = "Дата создания пользователя", example = "2007-12-03T10:15:30")
-    private final LocalDateTime createdAt;
+    private LocalDateTime createdAt;
     @Schema(description = "Дата последнего изменения пользователя", example = "2007-12-03T10:15:30.")
-    private final LocalDateTime updatedAt;
+    private LocalDateTime updatedAt;
 }
