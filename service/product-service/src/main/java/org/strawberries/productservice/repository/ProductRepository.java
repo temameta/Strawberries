b99538dd879104caca781445c32a26cdb823a8c5
tema <sezemov2@gmail.com>
2026-04-20 15:13:11 +0300

    feat: added product repository to product microservice

---
+++ service/product-service/src/main/java/org/strawberries/productservice/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package org.strawberries.productservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.strawberries.productservice.entity.ProductEntity;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductEntity, UUID> {
+}
