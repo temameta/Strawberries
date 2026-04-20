10243890822c14adce95c47e7fe8f31393244291
tema <sezemov2@gmail.com>
2026-04-20 15:12:09 +0300

    feat: added scalar registration to product microservice

---
+++ service/product-service/src/main/java/org/strawberries/productservice/scalar/ScalarRegistration.java
@@ -0,0 +1,17 @@
+package org.strawberries.productservice.scalar;
+
+import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsRuntimeWiring;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.scalars.ExtendedScalars;
+
+@DgsComponent
+public class ScalarRegistration {
+    @DgsRuntimeWiring
+    public RuntimeWiring.Builder addScalars(RuntimeWiring.Builder builder) {
+        return builder
+                .scalar(ExtendedScalars.GraphQLBigDecimal)
+                .scalar(ExtendedScalars.UUID)
+                .scalar(ExtendedScalars.DateTime);
+    }
+}
