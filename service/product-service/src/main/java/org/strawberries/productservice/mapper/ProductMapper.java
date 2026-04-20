90fb5f285348467bc7c7dcf789fa280ff6bd3cd4
tema <sezemov2@gmail.com>
2026-04-20 15:12:45 +0300

    feat: added product mapper to product microservice

---
+++ service/product-service/src/main/java/org/strawberries/productservice/mapper/ProductMapper.java
@@ -0,0 +1,27 @@
+package org.strawberries.productservice.mapper;
+
+import org.mapstruct.*;
+import org.strawberries.productapi.codegen.types.CreateProductInput;
+import org.strawberries.productapi.codegen.types.Product;
+import org.strawberries.productapi.codegen.types.UpdateProductInput;
+import org.strawberries.productservice.entity.ProductEntity;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface ProductMapper {
+    Product toGqlType(ProductEntity entity);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "active", constant = "true")
+    @Mapping(target = "category", ignore = true)
+    ProductEntity toEntityFromCreate(CreateProductInput input);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "category", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "active", ignore = true)
+    void updateEntity(UpdateProductInput input, @MappingTarget ProductEntity entity);
+}
