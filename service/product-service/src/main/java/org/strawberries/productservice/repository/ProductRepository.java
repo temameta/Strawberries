0c8a487078cf17539ace37323f692f1746c45408
Tema <sezemov2@gmail.com>
2026-04-20 18:16:34 +0300

    feat: added new methods to product repository in product microservice

--- service/product-service/src/main/java/org/strawberries/productservice/repository/ProductRepository.java
+++ service/product-service/src/main/java/org/strawberries/productservice/repository/ProductRepository.java
@@ -1,11 +1,17 @@
 package org.strawberries.productservice.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.strawberries.productservice.entity.ProductEntity;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, UUID> {
+    Page<ProductEntity> findAllByActive(Pageable pageable, Boolean active);
+
+    Optional<ProductEntity> findByIdAndActiveTrue(UUID id);
 }
