1469f6b9570af0bd0002219fb7362cdb58bfc1b3
Tema <sezemov2@gmail.com>
2026-04-21 22:28:57 +0300

    feat: added ProductDataFetcher

--- service/product-service/src/main/java/org/strawberries/productservice/fetcher/ProductDataFetcher.java
+++ service/product-service/src/main/java/org/strawberries/productservice/fetcher/ProductDataFetcher.java
@@ -1,8 +1,48 @@
 package org.strawberries.productservice.fetcher;
 
 import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsMutation;
+import com.netflix.graphql.dgs.DgsQuery;
+import com.netflix.graphql.dgs.InputArgument;
+import org.strawberries.productapi.codegen.types.CreateProductInput;
+import org.strawberries.productapi.codegen.types.Product;
+import org.strawberries.productapi.codegen.types.ProductCollection;
+import org.strawberries.productapi.codegen.types.UpdateProductInput;
+import org.strawberries.productservice.service.ProductService;
+
+import java.util.Objects;
+import java.util.UUID;
 
 @DgsComponent
-public class ProductDataFetcher {
+public record ProductDataFetcher(ProductService service) {
+    @DgsQuery
+    public Product product(@InputArgument UUID id) {
+        return service.findById(id, true);
+    }
 
+    @DgsQuery
+    public ProductCollection products(
+            @InputArgument Integer page,
+            @InputArgument Integer size) {
+        page = Objects.requireNonNullElse(page, 0);
+        size = Objects.requireNonNullElse(size, 20);
+        return service.findAll(page, size, true);
+    }
+
+    @DgsMutation
+    public Product createProduct(@InputArgument CreateProductInput input) {
+        return service.create(input);
+    }
+
+    @DgsMutation
+    public Product updateProduct(
+            @InputArgument UUID id,
+            @InputArgument UpdateProductInput input) {
+        return service.update(id, input);
+    }
+
+    @DgsMutation
+    public Product deleteProduct(@InputArgument UUID id) {
+        return service.delete(id);
+    }
 }
