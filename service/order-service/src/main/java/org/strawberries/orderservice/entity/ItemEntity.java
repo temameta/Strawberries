e845fb7dabf8e9d0e0b009e2e8362ddbb509b402
tema <sezemov2@gmail.com>
2026-05-18 15:10:05 +0300

    fix: added getters and setters to item entity

--- service/order-service/src/main/java/org/strawberries/orderservice/entity/ItemEntity.java
+++ service/order-service/src/main/java/org/strawberries/orderservice/entity/ItemEntity.java
@@ -1,10 +1,14 @@
 package org.strawberries.orderservice.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.UUID;
 
 @Entity
+@Setter
+@Getter
 public class ItemEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
